b235be6694453f7d35268df74ee339351cebf03e
vagrant <vagrant@precise32.(none)>
2013-04-05 23:11:05 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hocpoc::Application.config.secret_token = '8ab6a53a7ce6bc2fb7eb98e5c14265da2d3ebd7fef894b7a13d6b0f68e7a61289c14fe8b3b4db296e369bc0087877685c57e95a1f69c6cf4d000852aa1a54eac'
