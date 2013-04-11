93750a4e9ad7919c37a0f618abbf4a9f8204dd44
Phil0xF7 <phil@phamlabs.com>
2013-04-10 21:05:33 -0700

    refactor index.html.erb

--- app/assets/javascripts/hometemp.js
+++ app/assets/javascripts/hometemp.js
@@ -1,5 +1,6 @@
 $(function () {
   // var data = [ { x: 0, y: 40 }, { x: 1, y: 49 }, { x: 2, y: 17 }, { x: 3, y: 42 } ];
+  var data = $('#chart').data('url');
   var graph = new Rickshaw.Graph( {
           element: document.querySelector("#chart"),
           width: 580,
