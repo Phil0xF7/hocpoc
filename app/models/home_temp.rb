d0c51116dbcdc25ba4a07314f05b46fb24f0c948
Phil0xF7 <phil@phamlabs.com>
2013-04-12 07:33:38 -0700

    add logging and timestamp support

--- app/models/home_temp.rb
+++ app/models/home_temp.rb
@@ -1,3 +1,12 @@
 class HomeTemp < ActiveRecord::Base
   attr_accessible :humidity, :temp
+
+  def self.retrieve_data
+    logger.info "#{Time.now} - HomeTemp.retrieve_data - Start"
+    logger.info "#{Time.now} - HomeTemp.retrieve_data - Write to DB"
+
+    HomeTemp.create(:humidity => rand(100), :temp => rand(100))
+
+    logger.info "#{Time.now} - HomeTemp.retrieve_data - End"
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Log Configuration
+require 'logger'
+logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log")
+logger.formatter = Logger::Formatter.new
+
 # Initialize the rails application
 Hocpoc::Application.initialize!
