e80f3f4479e0db01fdcc68b557470aba541a8ff1
Phil0xF7 <phil@phamlabs.com>
2013-04-12 08:08:09 -0700

    add thermostat support

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.13'
 gem 'thin'
 gem 'sqlite3'
 gem 'whenever'
+gem 'nest_thermostat'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/home_temp.rb
+++ app/models/home_temp.rb
@@ -3,9 +3,15 @@ class HomeTemp < ActiveRecord::Base
 
   def self.retrieve_data
     logger.info "#{Time.now} - HomeTemp.retrieve_data - Start"
-    logger.info "#{Time.now} - HomeTemp.retrieve_data - Write to DB"
 
-    HomeTemp.create(:humidity => rand(100), :temp => rand(100))
+    @nest = NestThermostat::Nest.new({email: ENV['NEST_EMAIL'], password: ENV['NEST_PASS']})
+
+    if @nest
+      logger.info "#{Time.now} - HomeTemp.retrieve_data - Write to DB"
+      HomeTemp.create(:temp => @nest.current_temp, :humidity => @nest.humidity)
+    else
+      logger.error "#{Time.now} - HomeTemp.retrieve_data - Error establishing connection"
+    end
 
     logger.info "#{Time.now} - HomeTemp.retrieve_data - End"
   end
