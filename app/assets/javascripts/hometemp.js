93551351241b4d292db231066b6196679a27e5b7
Phil0xF7 <phil@phamlabs.com>
2013-04-12 22:32:11 -0700

    update graph views

--- app/assets/javascripts/hometemp.js
+++ app/assets/javascripts/hometemp.js
@@ -5,11 +5,25 @@ $(function () {
           element: document.querySelector("#chart"),
           width: 580,
           height: 250,
+          max: 100,
+          renderer: 'line',
           series: [ {
+                  name: 'Temp',
                   color: 'steelblue',
                   data: data
           } ]
   } );
+
+  var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+    graph: graph,
+    formatter: function(series, x, y) {
+      var date = '<span class="date">' + new Date(x * 1000).toLocaleString() + '</span>';
+      var swatch = '<span class="detail_swatch" style="background-color: ' + series.color + '"></span>';
+      var content = swatch + series.name + ": " + parseInt(y, 10) + '<br>' + date;
+      return content;
+    }
+  } );
+
   var x_axis = new Rickshaw.Graph.Axis.Time( { graph: graph } );
 
   var y_axis = new Rickshaw.Graph.Axis.Y( {
